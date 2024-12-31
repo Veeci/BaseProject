5a0de9d019dc60d410e970d25bee45d142524200
quangdv.tt <doanvietquang15102003@gmail.com>
2024-12-31 16:01:28 +0700

    updated version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,7 +105,7 @@ afterEvaluate {
             register<MavenPublication>("release") {
                 groupId = "com.veeci"
                 artifactId = "base"
-                version = "1.0.0"
+                version = "1.0.2"
 
                 afterEvaluate {
                     from(components["release"])
